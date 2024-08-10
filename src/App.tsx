4a93c5f9cf1d483cf9d5e6db413c5b967ecc52d6
hiroki takada <hirorotaka5@gmail.com>
2024-08-10 21:35:48 +0900

    初期ルーティングの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Report from './pages/Report';
+import NoMatch from './pages/NoMatch';
 
 function App() {
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/report" element={<Report />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
