ce640139f752be1e66851dfc1048fa87422590e8
hiroki takada <hirorotaka5@gmail.com>
2024-08-11 14:28:47 +0900

    fullcalenderで使用するイベントハンドラの追加

--- src/hooks/useFullcalender.tsx
+++ src/hooks/useFullcalender.tsx
@@ -1,16 +1,23 @@
 import { Balance, CalenderContent } from '../types';
 import { EventContentArg, DatesSetArg } from '@fullcalendar/core';
+import { DateClickArg } from '@fullcalendar/interaction';
 import { formatCurrency } from '../utils/formatting';
+import { useTheme } from '@mui/material';
 
 interface useFullcalenderProps {
   dailyBalances: Record<string, Balance>;
   setCurrentMonth: React.Dispatch<React.SetStateAction<Date>>;
+  setCurrentDay: React.Dispatch<React.SetStateAction<string>>;
+  currentDay: string;
 }
 
 const useFullcalender = ({
   dailyBalances,
   setCurrentMonth,
+  setCurrentDay,
+  currentDay,
 }: useFullcalenderProps) => {
+  const theme = useTheme();
   // FullCalendar用のイベントを生成する関数
   const createCalendarEvents = (
     dailyBalances: Record<string, Balance>
@@ -48,14 +55,30 @@ const useFullcalender = ({
     );
   };
 
+  // 現在の月を選択したときの処理
   const handleDateSet = (datesetInfo: DatesSetArg) => {
     const currentMonth = datesetInfo.view.currentStart;
     setCurrentMonth(currentMonth);
   };
+
+  // 現在の日付を選択したときの処理
+  const handleDateClick = (dateInfo: DateClickArg) => {
+    setCurrentDay(dateInfo.dateStr);
+  };
+
+  // イベントの色を設定する関数
+  const backgroundEvent = {
+    start: currentDay,
+    display: 'background',
+    backgroundColor: theme.palette.incomeColor.light,
+  };
+
   return {
     calendarEvents,
     renderEventContent,
     handleDateSet,
+    handleDateClick,
+    backgroundEvent,
   };
 };
 
