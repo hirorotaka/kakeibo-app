027081618193e5b9649362cfb9dcdff2cf536301
hiroki takada <hirorotaka5@gmail.com>
2024-08-11 21:34:23 +0900

    日付選択エリアのJSX雛形作成

--- src/components/MonthSelector.tsx
+++ src/components/MonthSelector.tsx
@@ -1,5 +1,34 @@
+import { Box, Button } from '@mui/material';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { ja } from 'date-fns/locale';
+
 const MonthSelector = () => {
-  return <div>MonthSelector</div>;
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ja}>
+      <Box
+        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+      >
+        <Button color={'error'} variant="contained">
+          先月
+        </Button>
+        <DatePicker
+          label="年月を選択"
+          sx={{ mx: 2, backgroundColor: 'white' }}
+          views={['year', 'month']}
+          format="yyyy/MM"
+          slotProps={{
+            toolbar: { toolbarFormat: 'yyy/MM', hidden: false },
+            calendarHeader: { format: 'yyyy/MM' },
+          }}
+        />
+        <Button color={'primary'} variant="contained">
+          翌月
+        </Button>
+      </Box>
+    </LocalizationProvider>
+  );
 };
 
 export default MonthSelector;
