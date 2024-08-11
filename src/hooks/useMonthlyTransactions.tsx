b1997c2b64d8569635986e1debd40cc37d50aa8a
hiroki takada <hirorotaka5@gmail.com>
2024-08-11 18:53:30 +0900

    月ごとのデータの取得をhook化

---
+++ src/hooks/useMonthlyTransactions.tsx
@@ -0,0 +1,20 @@
+import { useMemo } from 'react';
+import { useAppContext } from '../context/AppContext';
+import { formatMonth } from '../utils/formatting';
+import { Transaction } from '../types';
+
+const useMonthlyTransactions = (): Transaction[] => {
+  const { transactions, currentMonth } = useAppContext();
+
+  const monthlyTransactions = useMemo(
+    () =>
+      transactions.filter((transaction) =>
+        transaction.date.startsWith(formatMonth(currentMonth))
+      ),
+    [transactions, currentMonth]
+  );
+
+  return monthlyTransactions;
+};
+
+export default useMonthlyTransactions;
