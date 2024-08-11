c0dbf54c03990dfbcf566e55685d907e7d87c16f
hiroki takada <hirorotaka5@gmail.com>
2024-08-11 23:00:09 +0900

    棒グラフの表示機能の追加

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,5 +1,88 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartData,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { calculateDailyBalance } from '../utils/financeCalculations';
+import { Typography, useTheme } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import useMonthlyTransactions from '../hooks/useMonthlyTransactions';
+import { useAppContext } from '../context/AppContext';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 const BarChart = () => {
-  return <div>BarChart</div>;
+  const monthlyTransactions = useMonthlyTransactions();
+  const { isLoading } = useAppContext();
+  const theme = useTheme();
+  const options = {
+    maintainAspectRatio: false,
+    responsive: true,
+    plugins: {
+      legend: {
+        // position: 'left' as const,
+      },
+      title: {
+        display: true,
+        text: '日別収支',
+      },
+    },
+  };
+
+  const dailyBalances = calculateDailyBalance(monthlyTransactions);
+
+  const dateLabels = Object.keys(dailyBalances).sort();
+  const expenseData = dateLabels.map((day) => dailyBalances[day].expense);
+  const incomeData = dateLabels.map((day) => dailyBalances[day].income);
+
+  const data: ChartData<'bar'> = {
+    labels: dateLabels,
+    datasets: [
+      {
+        label: '支出',
+        data: expenseData,
+        backgroundColor: theme.palette.expenseColor.light,
+      },
+      {
+        label: '収入',
+        data: incomeData,
+        backgroundColor: theme.palette.incomeColor.light,
+      },
+    ],
+  };
+
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      {isLoading ? (
+        <CircularProgress />
+      ) : monthlyTransactions.length > 0 ? (
+        <Bar options={options} data={data} />
+      ) : (
+        <Typography>データはありません</Typography>
+      )}
+    </Box>
+  );
 };
 
 export default BarChart;
